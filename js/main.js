eeb6c0089a8692be26d0d5ae571053c206c07e66
Zhang Xin <zhangxindev@gmail.com>
2020-05-01 01:27:56 +0800

    完成基础功能

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 // 声明键盘字母数组
-var btnArray = [
+let btnArray = [
   ['q','w','e','r','t','y','u','i','o','p'],
   ['a','s','d','f','g','h','j','k','l'],
   ['z','x','c','v','b','n','m']
 ]
 
 // 声明 hash
-var btnWebsiteHash = {
+let btnWebsiteHash = {
   q:'qq.com',
   w:'weibo.com',
   a:'acfun.cn',
@@ -14,19 +14,49 @@ var btnWebsiteHash = {
   b:'bilibili.com',
   m:'meituan.com'
 }
+// 未解决 bug
+let hashInLocalStorage = JSON.parse(localStorage.getItem('localWebsiteHash') || '')
+if(hashInLocalStorage) {
+  btnWebsiteHash = hashInLocalStorage
+}
 
 // 生成按钮元素并放入页面
 for (let i = 0; i < btnArray.length; i++) {
-  btnline = document.createElement('div')
-  btnline.className = 'btnline ' + 'line' + (i + 1) + ' clearfix'
+  let btnline = document.createElement('div')
+  btnline.className = 'btnline ' + 'line' + (i + 1) + ' clearfix' 
+
   for (let j = 0; j < btnArray[i].length; j++) {
-    btn = document.createElement('kbd')
-    btn.className = 'btn'
-    btn.innerText = btnArray[i][j]
-    btnline.append(btn)
+    // 生成键盘按键
+    let keystoke = document.createElement('kbd')
+    keystoke.className = 'btn'
+    keystoke.innerText = btnArray[i][j]
+
+    // 生成编辑按钮并将其 id 设置为键盘的字符
+    let editBtn = document.createElement('button')
+    editBtn.innerText = 'M'
+    editBtn.id = keystoke.innerText
+    // 添加编辑网站功能
+    editBtn.onclick = function() {
+      let key = this.id
+      let addedWebsite = prompt('请添加一个网址')
+      btnWebsiteHash[key] = addedWebsite
+      localStorage.setItem('localWebsiteHash', JSON.stringify(btnWebsiteHash))
+    }
+
+    keystoke.append(editBtn)
+    btnline.append(keystoke)
   }
+
   keyboard.append(btnline)
 }
 
 // 给按钮添加交互
+document.onkeydown = function(event) {
+  let btnKey = event.key
+  let website = btnWebsiteHash[btnKey]
+  if(website) {
+    window.open('http://' + website,'_blank')
+  }
+}
+
 
